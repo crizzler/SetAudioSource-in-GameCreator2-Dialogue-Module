1903f7058a45d4bdfbdeb5ed32be2ed18ca0f186
Arawn <c.peterwerth@gmail.com>
2023-03-11 13:22:02 +0900

    Add files via upload

---
+++ InstructionStopAudioSource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using GameCreator.Runtime.Common;
+using GameCreator.Runtime.Common.Audio;
+using GameCreator.Runtime.VisualScripting;
+using UnityEngine;
+
+[Version(1, 0, 0)]
+
+[Title("StopsAudioSource")]
+[Description("Stops an AudioSource from playing a clip")]
+
+[Category("Audio/Stop AudioSource")]
+
+[Parameter("Target", "A game object that is set as the AudioSource")]
+
+[Keywords("Audio", "Voice", "Voices", "Sounds", "Character", "Silence", "Mute", "Fade")]
+[Image(typeof(IconVolume), ColorTheme.Type.TextLight, typeof(OverlayCross))]
+
+[Serializable]
+public class InstructionStopAudioSource : Instruction
+{
+    [SerializeField] private PropertyGetGameObject m_AudioSourceGameObject = new PropertyGetGameObject();
+
+    public override string Title => $"Stop {this.m_AudioSourceGameObject} AudioSource";
+
+    protected override Task Run(Args args)
+    {
+        GameObject target = this.m_AudioSourceGameObject.Get(args);
+        AudioSource audioSource = target.GetComponent<AudioSource>();
+
+        audioSource.Stop();
+        return DefaultResult;
+    }
+}
